119042eae02fb0ac82c0f274b7fe6dc05120b1f0
Tom Thorogood <me+github@tomthorogood.co.uk>
2016-07-08 12:51:06 +0930

    Comment change for golint [ci skip]

--- internal/xor/xor.go
+++ internal/xor/xor.go
@@ -48,7 +48,7 @@ func safeXORBytes(dst, a, b []byte) int {
 	return n
 }
 
-// xorBytes xors the bytes in a and b. The destination is assumed to have enough
+// Bytes xors the bytes in a and b. The destination is assumed to have enough
 // space. Returns the number of bytes xor'd.
 func Bytes(dst, a, b []byte) int {
 	if supportsUnaligned {
